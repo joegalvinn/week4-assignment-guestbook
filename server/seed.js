4560a02aaa002c1965db2d8bd6e5c0ad5a601c85
Joseph <joseph.galvin@digisolve.co.uk>
2024-09-20 11:34:23 +0100

    table made

--- server/seed.js
+++ server/seed.js
@@ -1,3 +1,11 @@
 //here you can add your SQL queries to create your table and add dummy data
 
 import { db } from "./server.js";
+
+db.query(`CREATE TABLE IF NOT EXISTS reviews (
+    id SERIAL PRIMARY KEY,
+    name TEXT,
+    date DATE,
+    review VARCHAR(255),
+    star TEXT
+);`);
