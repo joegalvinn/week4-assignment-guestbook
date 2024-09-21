d458a450979cf06100c1fa7be765b6b2f6065fdd
Joseph <joseph.galvin@digisolve.co.uk>
2024-09-21 20:37:35 +0100

    cors police x3

--- server/server.js
+++ server/server.js
@@ -7,7 +7,11 @@ dotenv.config();
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://week4-assignment-guestbook-1.onrender.com",
+  })
+);
 
 app.options("*", cors());
 
