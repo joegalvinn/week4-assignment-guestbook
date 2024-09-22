a8275c9d468f41bef422dffb740cc9106b6b8d48
Joseph <joseph.galvin@digisolve.co.uk>
2024-09-22 09:51:51 +0100

    cors

--- server/server.js
+++ server/server.js
@@ -8,14 +8,8 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(cors());
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+app.options("*", cors());
+
 // const dbConnectionString = process.env.DATABASE_URL;
 
 // export const db = new pg.Pool({
